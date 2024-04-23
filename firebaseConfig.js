821346f9eef09d757c0ea2f583718568b35bbd15
gregoireDedeyne <gregoire.dedeyne@gmail.com>
2024-04-23 19:25:51 +0200

    change env

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,6 +1,6 @@
 export default {
-	apiKey: "AIzaSyCzkOmjM0sTFAZFT9ukcYK_WDLfz79PUfs",
-	authDomain: "weather app",
-	projectId: "Yweather-app-fa34e",
-	appId: "506605639679",
+	apiKey: env.apiKey,
+	authDomain: env.authDomain,
+	projectId: env.projectId,
+	appId: env.appId,
 };
